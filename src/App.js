6508de1187dee6fb47aba6294849a9e9c705ee17
James <clementjaymes@.com>
2021-10-29 07:31:56 -0700

    created components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ function App() {
   return (
     <div className="container">
       <h1 >Ou<span className="underline">r Me</span>nu</h1>
+      <div className="buttonContainer">
+        <button className="buttonProps">All</button>
+        <button className="buttonProps">Breakfast</button>
+        <button className="buttonProps">Launch</button>
+        <button className="buttonProps">Shakes</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/Recipe.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+function Recipe() {
+  return (
+    <div>
+      3
+    </div>
+  )
+}
+
+export default Recipe
