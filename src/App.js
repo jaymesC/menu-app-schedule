b2d4c011d3ee4663d54521344c7d0884b59aad53
James <clementjaymes@.com>
2021-10-30 17:20:55 -0700

    updated components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
+import React from "react";
 import Recipe from "./components/Recipe";
 import { data } from "./data";
 
 function App() {
+  const [click, setClick] = React.useState(data);
+
+  const clickHandler = () => {
+    let test = click.filter((click) => click.time === "Breakfast")
+    setClick(test)
+    console.log(test);
+    
+  }
+
+  const lunchFunc = () => {
+    let lunch = click.filter(click => click.time === "Lunch")
+    setClick(lunch)
+    console.log(lunch);
+  }
+
   return (
     <div className="container">
       <h1 >Ou<span className="underline">r Me</span>nu</h1>
       <div className="buttonContainer">
-        <button className="buttonProps">All</button>
-        <button className="buttonProps">Breakfast</button>
-        <button className="buttonProps">Launch</button>
+        <button className="buttonProps" onClick={() => setClick(data)}>All</button>
+        <button className="buttonProps"  onClick={() => clickHandler(data.time)}>Breakfast</button>
+        <button className="buttonProps" onClick={() => lunchFunc(data.time)}>Launch</button>
         <button className="buttonProps">Shakes</button>
       </div>
 
       <div className="wrapper" key={data.id}>
-      {data.map((food) => (
+      {click.map((food) => (
         <div >
-        <Recipe {...food}/>  
+        <Recipe {...food} onClick={() => {clickHandler(); lunchFunc()}}/>  
         </div>
 
       ))}
--- src/data.js
+++ src/data.js
@@ -5,6 +5,7 @@ export const data = [
     price: '$15',
     title: 'Buttermilk Pancakes',
     description: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+    time:"Breakfast"
   },
   {
     id: 2,
@@ -12,6 +13,7 @@ export const data = [
     price: '$15',
     title: 'Diner Double',
     description: "vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats",
+    time:"Lunch"
   },
   {
     id: 3,
@@ -19,6 +21,7 @@ export const data = [
     price: '$15',
     title: 'Godzilla Milkshake',
     description: 'ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.',
+    time:"Shakes"
   },
   {
     id: 4,
@@ -26,6 +29,7 @@ export const data = [
     price: '$15',
     title: 'Country Delight',
     description: "Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut,",
+    time:"Breakfast"
   },
   {
     id: 5,
@@ -33,6 +37,7 @@ export const data = [
     price: '$15',
     title: 'Egg Attack',
     description: "franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up",
+    time:"Lunch"
   },
   {
     id: 6,
@@ -40,6 +45,7 @@ export const data = [
     price: '',
     title: 'Oreo Dream',
     description: 'Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday',
+    time:"Shakes"
   },
   {
     id: 7,
@@ -47,6 +53,7 @@ export const data = [
     price: '$15',
     title: 'Bacon Overflow',
     description: "carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird",
+    time:"Breakfast"
   },
   {
     id: 8,
@@ -54,6 +61,7 @@ export const data = [
     price: '$15',
     title: 'American Classic',
     description: 'on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut',
+    time:"Lunch"
   },
   {
     id: 9,
@@ -61,6 +69,7 @@ export const data = [
     price: '$15',
     title: 'Quarantine Buddy',
     description: 'skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.',
+    time:"Shakes"
   }
 
 ]
